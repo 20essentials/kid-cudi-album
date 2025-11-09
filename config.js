a607cf281d02c40d78a98af965e609e70acee47b
DominicDev <s0l0estudi4r@gmail.com>
2025-11-09 22:06:20 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'KID CUDI ';
+export const nameSongs = [
+	"PROJECT X PURSUIT OF HAPPINESS", /*1*/
+	"DAY 'N' NITE", /*2*/
+	"JUST WHAT I AM", /*3*/
+	"ERASE ME", /*4*/
+	"STARS IN THE SKY", /*5*/
+	"THE ADVENTURES OF MOON", /*6*/
+	"PURSUIT (MEGAFORCE VERSION)", /*7*/
+	"KING WIZARD", /*8*/
+	"SCOTT MESCUDI VS. THE WORLD", /*9*/
+	"REVOFEV", /*10*/
+	"DON’T PLAY THIS SONG", /*11*/
+	"WE AITE", /*12*/
+	"ASHIN’ KUSHER", /*13*/
+	"WILD’N CUZ I’M YOUNG", /*14*/
+	"THE MOOD", /*15*/
+	"MANIAC", /*16*/
+	"MR. RAGER", /*17*/
+	"THESE WORRIES", /*18*/
+	"THE END", /*19*/
+	"ALL ALONG", /*20*/
+]
